3f44d52854ebab9fd49b71b5c24e986167388bb2
Itai Tenenbaum <taitulism@gmail.com>
2016-12-17 14:10:43 +0200

    map2json

--- test/structure.spec.js
+++ test/structure.spec.js
@@ -1,11 +1,12 @@
+'use strict';
+
 const expect = require('chai').expect;
 
 const structure = require('../index');
 
 describe('structure', () => {
-    it('is', () => {
-        const myModule = structure();
-
-        expect(myModule);
+    it('is an object with API methods', () => {
+        expect(structure).to.be.an.object;
+        expect(structure.mapToJson).to.be.a.function;
     });
 });
