4af351aa9c3941127632e02bdb173605774efcc6
Taitu Lizenbaum <taitulism@gmail.com>
2016-12-27 09:29:00 +0200

    add chmod property
    * chmod
    * getDefaultModeByPlatform

--- test/structure.spec.js
+++ test/structure.spec.js
@@ -1,9 +1,26 @@
 'use strict';
 
-const expect = require('chai').expect;
+const os          = require('os');
+const expect      = require('chai').expect;
 const resolvePath = require('path').resolve;
 
 const structure = require('../index');
+const platform = os.platform();
+
+const FOLDER = 0;
+const FILE   = 1;
+
+const winDefaultMode         = '666';
+const LinuxFileDefaultMode   = '644';
+const LinuxFolderDefaultMode = '755';
+
+function getDefaultModeByPlatform (platform, entryType = FILE) {
+    if (platform === 'win32') return winDefaultMode;
+
+    if (entryType === FILE) return LinuxFileDefaultMode;
+
+    return LinuxFolderDefaultMode;
+}
 
 describe('structure', () => {
     it('is an object', () => {
@@ -19,28 +36,34 @@ describe('structure', () => {
     describe('.toJson()', () => {
         it('maps a given folder to a JSON', (done) => {
             const expectedResult = {
+                mode: getDefaultModeByPlatform(platform, FOLDER),
                 path: resolvePath('./test/to-json'),
                 type: 0,
                 entries: {
                     'index.js': {
+                        mode: getDefaultModeByPlatform(platform, FILE),
                         path: resolvePath('test/to-json/index.js'),
                         type: 1
                     },
                     a: {
+                        mode: getDefaultModeByPlatform(platform, FOLDER),
                         path: resolvePath('test/to-json/a'),
                         type: 0,
                         entries: {
                             "index.js": {
+                                mode: getDefaultModeByPlatform(platform, FILE),
                                 path: resolvePath('test/to-json/a/index.js'),
                                 type: 1
                             }
                         }
                     },
                     b: {
+                        mode: getDefaultModeByPlatform(platform, FOLDER),
                         path: resolvePath('test/to-json/b'),
                         type: 0,
                         entries: {
                             "index.js": {
+                                mode: getDefaultModeByPlatform(platform, FILE),
                                 path: resolvePath('test/to-json/b/index.js'),
                                 type: 1
                             }
