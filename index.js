0cee4c7ba8bcc8332bacfd954b2cc8c3f0e1fa84
Itai Tenenbaum <taitulism@gmail.com>
2019-10-14 11:41:41 +0300

    rename lib > src

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const mapEntry = require('./lib/map-entry');
-const {FILE, FOLDER} = require('./lib/constants');
+const mapEntry = require('./src/map-entry');
+const {FILE, FOLDER} = require('./src/constants');
 
 mapEntry.FILE = FILE;
 mapEntry.FOLDER = FOLDER;
